2623abeed8902087f775553be1d79d9a81dfae4b
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-01-22 17:18:18 +0100

    Add not operator

--- src/TSQuery/Query.hs
+++ src/TSQuery/Query.hs
@@ -13,11 +13,13 @@ instance Category Entity where
   (Entity a) . (Entity b) = Entity (a `T.append` "." `T.append` b)
 
 data Query a = forall b. Eq b  => QEq (Entity a b) b
+             | forall b. Eq b  => QNEq (Entity a b) b
              | forall b. Ord b => QGrt (Entity a b) b
              | forall b. Ord b => QLs (Entity a b) b
              | forall b. Eq b  => QIn (Entity a [b]) b
              |                    QOr (Query a) (Query a)
              |                    QAnd (Query a) (Query a)
+             |                    QNot (Query a)
 
 eq :: Eq b => Entity a b -> b -> Query a
 eq = QEq
