eafb9ca473e10281d531cf077bba58820875c39f
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-01-23 20:23:02 +0100

    Bson dependency :(

--- src/TSQuery/Query.hs
+++ src/TSQuery/Query.hs
@@ -5,6 +5,7 @@ module TSQuery.Query where
 import           Prelude hiding ((.))
 import           Control.Category
 
+import           Data.Bson
 import qualified Data.Text                    as T
 
 data Entity a b = Entity T.Text deriving Show
@@ -19,26 +20,29 @@ instance Category Entity where
 showEntity :: Entity a b -> T.Text
 showEntity (Entity text) = text
 
-data Query a =        QAll
- | forall b. Eq  b => QEq  (Entity a b) b
- | forall b. Eq  b => QNeq (Entity a b) b
- | forall b. Ord b => QGrt (Entity a b) b
- | forall b. Ord b => QLs  (Entity a b) b
- | forall b. Eq  b => QCnt (Entity a [b]) b
- |                    QOr  (Query a) (Query a)
- |                    QAnd (Query a) (Query a)
- |                    QNot (Query a)
+data Query a =                 QAll
+ | forall b. (Val b, Eq  b) => QEq  (Entity a b) b
+ | forall b. (Val b, Eq  b) => QNeq (Entity a b) b
+ | forall b. (Val b, Ord b) => QGrt (Entity a b) b
+ | forall b. (Val b, Ord b) => QLs  (Entity a b) b
+ | forall b. (Val b, Eq  b) => QCnt (Entity a [b]) b
+ |                             QOr  (Query a) (Query a)
+ |                             QAnd (Query a) (Query a)
+ |                             QNot (Query a)
 
-eq :: Eq b => Entity a b -> b -> Query a
+eq :: (Val b, Eq b) => Entity a b -> b -> Query a
 eq = QEq
 
-grt :: Ord b => Entity a b -> b -> Query a
+neq :: (Val b, Eq b) => Entity a b -> b -> Query a
+neq = QNeq
+
+grt :: (Val b, Ord b) => Entity a b -> b -> Query a
 grt = QGrt
 
-ls :: Ord b => Entity a b -> b -> Query a
+ls :: (Val b, Ord b) => Entity a b -> b -> Query a
 ls = QLs
 
-contains :: Eq b => Entity a [b] -> b -> Query a
+contains :: (Val b, Eq b) => Entity a [b] -> b -> Query a
 contains = QCnt
 
 qall :: Query a
